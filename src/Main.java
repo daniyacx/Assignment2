028af720f9cdf39429e110cfcc94f3ae94de73d1
aubak <aubakirovadania@gmail.com>
2023-04-20 03:44:40 +0600

    MyLinkedList+documentation update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayList myArrayList = new MyArrayList();
-        myArrayList.add("apple");
-        myArrayList.add("banana");
-        myArrayList.add("orange");
-        myArrayList.add("apple");
-        myArrayList.add("grape");
-        System.out.println("Index of 'orange': " + myArrayList.indexOf("orange"));
-        System.out.println("Last index of 'apple': " + myArrayList.lastIndexOf("apple"));
+    //MyArrayList myArrayList = new MyArrayList();
+        //myArrayList.add("apple");
+        //myArrayList.add("banana");
+        //myArrayList.add("orange");
+        //myArrayList.add("apple");
+        //myArrayList.add("grape");
+        //System.out.println("Index of 'orange': " + myArrayList.indexOf("orange"));
+        //System.out.println("Last index of 'apple': " + myArrayList.lastIndexOf("apple"));
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(8, 0);
+        myLinkedList.add(5, 1);
+        myLinkedList.add(2, 2);
+        myLinkedList.add(4, 3);
+        System.out.println(myLinkedList.get(0));
+        System.out.println(myLinkedList.get(1));
+        System.out.println(myLinkedList.get(2));
+        System.out.println(myLinkedList.get(3));
     }
 }
